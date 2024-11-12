8ea6cda25dfc59ab3bd96b9ec16b004bf2ef52d3
kumabiko <nousbear.dev@gmail.com>
2024-11-12 14:45:42 +0900

    Expo Router Screens

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Image, Text, View } from "react-native";
+import { StyleSheet, Image, Text, View, Pressable } from "react-native";
+import { Link } from "expo-router";
 
 import Colors from "../constants/Colors";
 import { Product } from "../types";
@@ -9,19 +10,21 @@ type Props = {
 
 export const ProductListItem = ({ product }: Props) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri:
-            product.image ||
-            "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
-        }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-    </View>
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{
+            uri:
+              product.image ||
+              "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
+          }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -32,6 +35,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 10,
     overflow: "hidden",
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
